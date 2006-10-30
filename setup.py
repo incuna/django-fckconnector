622f5c3e8a1127a9ad26002aa396354f450df722
nathanyergler <nathanyergler@7ce0a19d-b11b-0410-b81a-571e2351ff3c>
2006-10-30 15:01:43 +0000

    Repository reorganization to support eggification.
    git-svn-id: http://django-fckconnector.googlecode.com/svn/trunk@7 7ce0a19d-b11b-0410-b81a-571e2351ff3c

---
+++ setup.py
@@ -0,0 +1,25 @@
+# XXX license header here
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "fckeditor.connector",
+    version = "0.1",
+    packages = find_packages('src'),
+    package_dir = {'':'src'},
+
+    # scripts and dependencies
+    install_requires = ['Django',
+                        ],
+    include_package_data = True,
+    namespace_packages = ['fckeditor'],
+    zip_safe = True,
+
+    # author metadata
+    author = 'Nathan R. Yergler',
+    author_email = 'nathan@yergler.net',
+    description = '.',
+    license = 'GNU LGPL',
+    url = 'http://code.google.com/p/django-fckconnector/',
+
+    )
